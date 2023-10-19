660063a02bd2e3733b286b001c54661ca207f98a
Damari <damaricruz650@gmail.com>
2023-10-18 22:19:24 -0600

    primer commit

---
+++ data.js
@@ -0,0 +1,41 @@
+let ListaTareas = [
+    {
+      id: 1,
+      nombre: "React",
+      fechaEntrega: 10/10,
+      modulo: 2,
+      estado:"realisado"
+      
+    },
+    {
+        id: 2,
+        nombre: "JS",
+        fechaEntrega: 10/11,
+        modulo: 2,
+        estado:"terminada"
+        
+      },
+      {
+        id: 3,
+        nombre: "CSS",
+        fechaEntrega: 25/10,
+        modulo: 1,
+        estado:"en proceso"
+      },
+      {
+        id: 4,
+        nombre: "JS",
+        fechaEntrega: 17/11,
+        modulo: 1,
+        estado:"realisado"
+        
+      },
+      {
+        id: 6,
+        nombre: "Nodejs",
+        fechaEntrega: 15/10,
+        modulo: 3,
+        estado:"terminada"
+        
+      },
+  ];
